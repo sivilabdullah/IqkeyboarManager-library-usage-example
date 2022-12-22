70e61d9d55f0a83ec0c75f762ac6a70644eb1962
sivilabdullah <194308002@ogr.ebyu.edu.tr>
2022-12-22 11:17:12 +0300

    Initial Commit

---
+++ IqkeyboarManager library usage example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IqkeyboarManager library usage example
+//
+//  Created by abdullah's Monterey on 22.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+    platform :ios, '16.2'
+
+target 'IqkeyboarManager library usage example' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IqkeyboarManager library usage example
+ pod 'IQKeyboardManagerSwift'
+end
